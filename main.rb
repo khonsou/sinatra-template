f8470ca1c5e0798156bf4fc4caec2481ffe96ac2
khonsou <newkhonsou@gmail.com>
2011-08-15 17:22:23 +0900

    skip config.ru for windows user

--- main.rb
+++ main.rb
@@ -1,14 +1,25 @@
-# -*- coding: utf-8 -*-
+# encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+require File.dirname(__FILE__)+'/helper'
+require File.dirname(__FILE__)+'/main'
+
+set :environemt, :production
+
 get '/' do
   @title = @@conf['title']
   haml :index
 end
 
 get '/omikuji.json' do
+
   content_type = 'application/json'
-  arr = ['大吉', '中吉', '小吉', 'マジキチ', '凶']
-  @mes = {
-    :result => arr.choice,
+
+  arr = ['a', 'b', 'c', 'd', 'e']
+
+  {
+    :result => arr.sample,
     :time => Time.now.to_s
   }.to_json
 end
